a80a3d181d391d49dca5458b17c7d6b2d69f3861
My Mistake <72628222+kierans-fluffypandas@users.noreply.github.com>
2021-09-24 10:28:13 -0600

    workperiod

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,26 +1,25 @@
 //GLOBAL VARI's
 let x;
 let y;
-let deg = 270;
+
 let speed = 3;
-let hit = true;
+let hit = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
   x = width / 2;
   y = height / 1.2;
 }
 
 function draw() {
-  background(255);
+  background(0);
   displayTarget();
   moveSight();
   collide();
 
 } 
 function collide() {
-  rect(200, 200, 100, 150);
+  //rect(200, 200, 100, 150);
   circle(mouseX, mouseY, 35);
  
   hit = collideRectCircle(200, 200, 100, 150, mouseX, mouseY, 35);
@@ -28,43 +27,28 @@ function collide() {
     stroke("red");
   }
   else {
-    stroke("black");
+    stroke("green");
   }
- 
-
 }
-
 
 function moveSight() {
   noFill();
   stroke(64);
   circle(mouseX, mouseY, 35);
   cursor(CROSS, [mouseX], [mouseY]);
- 
-
-
-  // if (keyIsDown(RIGHT_ARROW)) {
-  //   deg += 4;
-  // }
-  // if (keyIsDown(LEFT_ARROW)) {
-  //   deg -= 4;
-  // }
-  // if (keyIsDown(UP_ARROW)) {
-  //   x += cos(deg) * speed;
-  //   y += sin(deg) * speed;
-  // }
 }
 
 function displayTarget() {
+  stroke("green");
   push();
   circle(windowWidth/2, windowHeight/2, 75, 50, 50);
   circle(windowWidth/2, windowHeight/2, 125, 50, 50);
   circle(windowWidth/2, windowHeight/2, 175, 50, 50);
   circle(windowWidth/2, windowHeight/2, 225, 50, 50);
-  //rect(200, 200, 100, 150);
-  rect(1350, 200, 100, -150);
+  rect(200, 200, 100, 150);
+  rect(1400, 200, 100, -150);
   rect(50, 200, 100, -150);
-  rect(1200, 200, 100, 150);
+  rect(1250, 200, 100, 150);
   pop();
 }
 
