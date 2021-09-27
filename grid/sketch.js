0aea60a65cfc370e7b9bb9b59fc28073e4450e5d
Joyea2101254 <2101254@sps-student.sk.ca>
2021-09-27 11:01:51 -0600

    9/27/21 Grid

---
+++ grid/sketch.js
@@ -0,0 +1,67 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let grid;
+let gridSize = 10;
+
+function setup() {
+
+  if (windowHeight > windowWidth){
+    createCanvas(windowWidth, windowHeight);
+  }
+  else {
+  createCanvas(windowWidth, windowHeight);
+  }
+  grid = createRandGrid(gridSize);
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+function displayGrid() {
+  let cellSize = width/gridSize;
+
+  for (let y=0; y<grid.length; y++) {
+    for (let x=0; x<grid[y].length; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+
+function createEmptyGrid(howLarge) {
+  let emptyArray = [];
+  for (let y=0; y<howLarge; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<howLarge; x++) {
+      emptyArray[y].push(0);
+    }
+
+  }
+  return emptyArray;
+}
+function createRandGrid(howLarge) {
+  let emptyArray = [];
+  for (let y=0; y<howLarge; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<howLarge; x++) {
+      if (random(0, 100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  } 
+  return emptyArray;
+}
