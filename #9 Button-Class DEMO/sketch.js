226a85a6b9ea4b3920f4d591e7d826fa20874ee3
My Mistake <72628222+kierans-fluffypandas@users.noreply.github.com>
2021-10-12 10:58:53 -0600

    button update

--- #9 Button-Class DEMO/sketch.js
+++ #9 Button-Class DEMO/sketch.js
@@ -8,8 +8,20 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
   background(220);
+}
+
+class Button {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.hovercolor = "#DA627D";
+    this.notHoverColor = "#9A348E";
+  }
 }
