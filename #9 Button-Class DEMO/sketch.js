020fe3e346a1401a6af07204d47f9f0b66c014e5
My Mistake <72628222+kierans-fluffypandas@users.noreply.github.com>
2021-10-20 10:12:46 -0600

    button program

--- #9 Button-Class DEMO/sketch.js
+++ #9 Button-Class DEMO/sketch.js
@@ -1,18 +1,31 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+//button
 
+
+let bgColor = "white";
+
+let buttonOne, buttonTwo;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  buttonOne = new Button(300, 200, 400, 100);
+  buttonTwo = new Button(300, 400, 400, 100);
+  buttonTwo.notHoveredColor = "#DBFE87"; 
+  buttonTwo.hovercolor = "#FFE381"; 
 }
 
 function draw() {
-  background(220);
+  background(bgColor);
+  buttonOne.display();
+  buttonTwo.display();
+}
+
+function mousePressed() {
+  if (buttonOne.checkIfInside(mouseX, mouseY)) {
+    bgColor= "yellow";
+  }
+  else if (buttonTwo.checkIfInside(mouseX, mouseY)) {
+    bgColor = "black";
+  }
 }
 
 class Button {
@@ -21,7 +34,22 @@ class Button {
     this.y = y;
     this.width = w;
     this.height = h;
-    this.hovercolor = "#DA627D";
-    this.notHoverColor = "#9A348E";
+    this.hoverColor = "green";
+    this.notHoveredColor = "purple";
+  }
+
+  display() {
+    if(this.checkIfInside(mouseX, mouseY)) {
+      fill(this.hoverColor);
+    }
+    else {
+      fill(this.notHoveredColor);
+    }
+    rect(this.x, this.y, this.width, this.height);
+  }
+
+  checkIfInside(x, y) {
+    return x >= this.x && x <= this.x + this.width &&
+           y >= this.y && y <= this.y + this.height;
   }
 }
