593bf117881bcdf191d9a4ef2ffc8dc23eda7ed4
My Mistake <kieranj030@icloud.com>
2022-03-15 20:34:09 -0500

    yup

---
+++ web development/yup/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  line(1, 1, 5, 5);
+}
+
+function draw() {
+  background(220);
+}
